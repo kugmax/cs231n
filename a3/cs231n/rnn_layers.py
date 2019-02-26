7e1096e445fab15a1d2658492cb9830b8ff04726
Maxim Kugatov <maximkugatov@gmail.com>
2019-02-26 15:44:46 +0200

    Changes to be committed: 	modified:   RNN_Captioning.ipynb 	modified:   cs231n/rnn_layers.py

--- a3/cs231n/rnn_layers.py
+++ a3/cs231n/rnn_layers.py
@@ -214,7 +214,10 @@ def word_embedding_forward(x, W):
     #                                                                            #
     # HINT: This can be done in one line using NumPy's array indexing.           #
     ##############################################################################
-    pass
+
+    out = W[x, :]
+    cache = x, W
+
     ##############################################################################
     #                               END OF YOUR CODE                             #
     ##############################################################################
@@ -243,7 +246,13 @@ def word_embedding_backward(dout, cache):
     # Note that words can appear more than once in a sequence.                   #
     # HINT: Look up the function np.add.at                                       #
     ##############################################################################
-    pass
+
+    x, W = cache
+
+    dW = np.zeros(W.shape)
+
+    np.add.at(dW, x, dout)
+
     ##############################################################################
     #                               END OF YOUR CODE                             #
     ##############################################################################
